f89a46f8e0d0a2a7189e083d51d2d088042f0e25
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-05-23 00:58:02 -0500

    Add further functionality and fix bugs
    Now able to delete books and update the read/unread status. Also fixed
    the bug where cards were duplicating.

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function Book(title, author, numberOfPages, status){
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages; // should be a number in numeric chars
-    this.status = status; // should be 'read' or 'unread'
+    if (status === true){
+        this.status = "read";
+    } else {
+        this.status = "unread"
+    }
 } 
 
 function addBook(title, author, numberOfPages, status){
@@ -15,7 +19,35 @@ function addBook(title, author, numberOfPages, status){
 // loop through array to display books in library
 function displayBooks(){
     let container = document.querySelector(".book-container");
-    
+    // clear container 
+    container.innerHTML='';
+
+    // insert add-book-tile
+    let addBookTile = document.createElement("div");
+    let addBookTileContent = document.createElement("div");
+    let addBookBtn = document.createElement("button");
+    let addBookParagraph = document.createElement("p");
+
+    addBookTile.className = "add-book-tile";
+    addBookTileContent.className = "add-book";
+    addBookBtn.className = "add-btn";
+    addBookBtn.textContent = "+";
+    addBookParagraph.textContent = "Add Book";
+
+    addBookTileContent.appendChild(addBookBtn);
+    addBookTileContent.appendChild(addBookParagraph);
+    addBookTile.appendChild(addBookTileContent);
+    container.appendChild(addBookTile);
+
+    let addBtn = document.querySelector(".add-btn");
+    addBtn.addEventListener("click", () => {
+        let addBookForm = document.querySelector(".add-book-form");
+        addBookForm.style.visibility = "visible";
+    });
+
+
+    let index = 0;
+
     for (book of library){
         let bookTile = document.createElement("div");
         let title = document.createElement("p");
@@ -24,7 +56,8 @@ function displayBooks(){
         let status = document.createElement("button");
         let deleteButton = document.createElement("button");
 
-        bookTile.classList.add("book-tile");
+        bookTile.className = "book-tile";
+        bookTile.id = index;
         title.textContent = book.title;
         title.className = "book-title";
         author.textContent = book.author;
@@ -32,9 +65,11 @@ function displayBooks(){
         pages.textContent = `${book.numberOfPages} pages`;
         pages.className = "book-pages";
         status.textContent = book.status;
-        status.className = "book-status";
+        status.classList.add(`status-btn-${index}`);
+        status.classList.add("book-status");
         deleteButton.textContent = "delete";
-        deleteButton.className = "delete-btn";
+        deleteButton.classList.add(`delete-btn-${index}`);
+        deleteButton.classList.add("delete-btn"); 
 
         bookTile.appendChild(title);
         bookTile.appendChild(author);
@@ -43,35 +78,24 @@ function displayBooks(){
         bookTile.appendChild(deleteButton);
 
         container.appendChild(bookTile);
-      
+
+        if (library.length >= 1){
+            let deleteBtn = document.querySelector(`.delete-btn-${index}`);
+            deleteBtn.addEventListener("click", (event) => {
+                let bookCard = event.target.parentNode.id;
+                deleteBook(Number(bookCard));
+            });
+
+            let statusBtn = document.querySelector(`.status-btn-${index}`);
+            statusBtn.addEventListener("click", (event) => {
+                let libraryIndex = event.target.parentNode.id;
+                console.log("CLICKED!!")
+                updateStatus(libraryIndex);
+            })
+        }
+        index += 1;
     }
 }
-
-window.addEventListener("load", () => {
-    displayBooks();
-  });
-
-let addBtn = document.querySelector(".add-btn");
-addBtn.addEventListener("click", () => {
-    let addBookForm = document.querySelector(".add-book-form");
-    addBookForm.style.visibility = "visible";
-});
-
-
-
-/*let addBookForm = document.querySelector(form);
-addBookForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let bookTitle = document.querySelector("#title").value;
-    let bookAuthor = document.querySelector("#author").value;
-    let bookLength = document.querySelector("#number_of_pages").value;
-    let bookStatus = document.getElementsByName("status");
-
-    addBook(bookTitle, bookAuthor, bookLength, "read");
-    displayBooks();
-    let addBookForm = document.querySelector(".add-book-form");
-    addBookForm.style.visibility = "hidden";
-});*/
 
 
 
@@ -80,19 +104,31 @@ function addToLibrary(event){
     let bookTitle = document.getElementById("title").value;
     let bookAuthor = document.getElementById("author").value;
     let bookLength = document.getElementById("number_of_pages").value;
-    let bookStatus = document.getElementsByName("status");
+    let bookStatus = document.getElementsByName("status")[0].checked;
 
-    addBook(bookTitle, bookAuthor, bookLength, "read");
+    addBook(bookTitle, bookAuthor, bookLength, bookStatus);
     displayBooks();
     let addBookForm = document.querySelector(".add-book-form");
     addBookForm.style.visibility = "hidden";
     event.preventDefault(); 
 }
 
-let deleteBtn = document.querySelector(".delete-btn");
-deleteBtn.addEventListener("click", () => {
-    return console.log("DELETE BUTTON IN PROGRESS")
+
+function deleteBook(index){
+    library.splice(index, 1);
+    let tempLibrary = library;
+    displayBooks();
+}
+
+function updateStatus(index){
+    if (library[index].status === "read"){
+        library[index].status = "unread";
+    } else {
+        library[index].status = "read";
+    }
+    displayBooks();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    displayBooks();
 });
-
-
-// add a button to change the status of the book
